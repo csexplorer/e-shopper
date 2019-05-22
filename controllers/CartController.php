f5891fa47067965b4ce7e7e37d36ff9120e3e909
Salim ibn Usmon <salimibnusmon@gmail.com>
2019-05-22 06:40:03 +0500

    order product finished

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItem;
 use Yii;
 
 /**
@@ -57,6 +59,38 @@ class CartController extends AppController {
     }
 
     public function actionView() {
-        return $this->render('view');
+        $session = Yii::$app->session;
+        $session->open();
+        $order = new Order();
+
+        if ($order->load(Yii::$app->request->post())) {
+            $order->qty = $session['cart.qty'];
+            $order->sum = $session['cart.sum'];
+
+            if ($order->save()) {
+                $this->saveOrderItems($session['cart'], $order->id);
+                Yii::$app->session->setFlash('success', 'Your order successfully sent.');
+                $session->remove('cart');
+                $session->remove('cart.qty');
+                $session->remove('cart.sum');
+                return $this->refresh();
+            } else {
+                Yii::$app->session->setFlash('fail', 'Your order failed.');
+            }
+        }
+        return $this->render('view', compact('session', 'order'));
+    }
+
+    protected function saveOrderItems($items, $order_id) {
+        foreach($items as $id => $item) {
+            $order_item = new OrderItem();
+            $order_item->order_id = $order_id;
+            $order_item->product_id = $id;
+            $order_item->name = $item['name'];
+            $order_item->price = $item['price'];
+            $order_item->qty_item = $item['qty'];
+            $order_item->sum_item = $item['qty'] * $item['price'];
+            $order_item->save();
+        }
     }
 }
--- views/cart/view.php
+++ views/cart/view.php
@@ -1,1 +1,85 @@
-Cart
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+
+?>
+
+<div class="container">
+  <div class="row">
+      <?php if (Yii::$app->session->hasFlash('success')) : ?>
+        <div class="alert alert-success"><?= Yii::$app->session->getFlash('success') ?></div>
+      <?php endif; ?>
+
+      <?php if (Yii::$app->session->hasFlash('fail')) : ?>
+        <div class="alert alert-danger"><?= Yii::$app->session->getFlash('fail') ?></div>
+      <?php endif; ?>
+  </div>
+</div>
+
+<div class="container" style="border: 1px solid #ccc; margin-bottom: 50px; padding-bottom: 15px;">
+  <?php if (!empty($session['cart'])) : ?>
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="text-center">Your cart</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="table-responsive">
+          <table class="table table-bordered table-hover table-stripped">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Price</th>
+                <th>Total Price</th>
+                <th><span class="glyphicon glyphicon-remove"></span></th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($session['cart'] as $id => $item) : ?>
+                <tr>
+                  <td><?= Html::img("/images/products/{$item['img']}", ['alt' => $item['name'], 'height' => 50]) ?></td>
+                  <td><a href="<?= Url::to(['product/view', 'id' => $id]) ?>"><?= $item['name'] ?></a></td>
+                  <td><?= $item['qty'] ?></td>
+                  <td>$<?= $item['price'] ?></td>
+                  <td>$<?= $item['qty'] * $item['price'] ?></td>
+                  <td><span class="glyphicon glyphicon-remove text-danger del-cart-item" data-id="<?= $id ?>"></span></td>
+                </tr>
+              <?php endforeach; ?>
+              <tr>
+                <td colspan="5" align="right">Total Count : </td>
+                <td><?= $session['cart.qty'] ?></td>
+              </tr>
+              <tr>
+                <td colspan="5" align="right">Total Sum  : </td>
+                <td>$<?= $session['cart.sum'] ?></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <hr>
+
+        <h2 class="text-center">Order Form</h2>
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($order, 'name')->textInput() ?>
+        <?= $form->field($order, 'email')->textInput() ?>
+        <?= $form->field($order, 'phone')->textInput() ?>
+        <?= $form->field($order, 'address')->textInput() ?>
+
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-success btn-block']) ?>
+
+        <?php $form = ActiveForm::end(); ?>
+      </div>
+    </div>
+      
+  <?php else : ?>
+    <h2>Cart is emtpty</h2>
+  <?php endif; ?>
+</div>
